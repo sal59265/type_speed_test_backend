0bfaf18a0dc76a6dae25b26ff50265cf4eef97b8
Soul Lee <lee.soul12@gmail.com>
2022-02-13 16:27:12 -0500

    install buildpack

--- config/config.js
+++ config/config.js
@@ -10,6 +10,11 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
+    version: '1.0.0',
+    engines: {
+      npm: '8.x',
+      node: '16.x'
+    },
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
